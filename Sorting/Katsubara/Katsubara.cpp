c05014f6a0b7189952a288f8029d262add454f4d
Nick Thornton <n.419@live.com>
2017-03-03 17:36:44 +0000

    Vectors

--- Sorting/Katsubara/Katsubara.cpp
+++ Sorting/Katsubara/Katsubara.cpp
@@ -4,13 +4,16 @@
 #include <sstream>
 #include <ios>
 #include <iomanip>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+using big = long double;
 
 string ss1{ R"(3141592653589793238462643383279502884197169399375105820974944592)" };
 string ss2{ R"(2718281828459045235360287471352662497757247093699959574966967627)" };
-string s1{ R"(12345)" };
-string s2{ R"(56778)" };
+string s1{ R"(1234)" };
+string s2{ R"(4321)" };
 
 /*
 
@@ -30,66 +33,133 @@ return P1 * 2^n + (P3 - P1 - P2)  2^(n/2) + P2
 
 */
 
-unsigned long long power(unsigned x) {
+unsigned power(unsigned x) {
 	int p{ 1 };
 	while (x--) p *= 10;
 	return p;
 }
 
-unsigned long long Karatsuba(string s1, string s2 ) {
-	if (s1.length() == 1 && s2.length() == 1) {  
-		long A{ atol(s1.c_str()) };
-		long B{ atol(s2.c_str()) };
-		long result = A*B;
-		return result;
+// splits the charcters of number into two 
+void splitter(big number, big & a, big & b) {
+	a = 0; b = 0;
+	stringstream ss;
+	ss << number;
+	string s{ ss.str() };
+	size_t half{ s.length() };
+	half = half / 2 + (half % 2);
+	string aString(s.substr(0, half));
+	string bString(s.substr(half));
+	cout << fixed << setprecision(0);
+	//cout << s << " => " << aString << " & " << bString << endl;
+	ss.clear();
+	ss.str(aString);
+	ss >> a;
+	ss.clear();
+	ss.str(bString);
+	ss >> b;
+	cout << fixed << setprecision(0);
+	//cout << number << " => " << a << " & " << b << endl;
+}
+
+big tenToTheN(big number) {
+	stringstream ss;
+	ss << number;
+	string s{ ss.str() };
+	return power(s.length());
+}
+
+big tenToTheN2(big number) {
+	stringstream ss;
+	ss << number;
+	string s{ ss.str() };
+	unsigned len{ s.length() };
+	len = len / 2 + (len % 2);
+	return power(s.length() >> 1);
+}
+
+big digitsIn(big number) {
+	stringstream ss;
+	ss << number;
+	string s{ ss.str() };
+	return s.length();
+}
+
+big Karatsuba(big n1, big n2) {
+	if (digitsIn(n1) == 1 && digitsIn(n2) == 1) {
+		return n1 * n2;
 	}
 
-	//cout << s1 << " x " << s2 << "\n";
-	auto n11{ s1.length() };
-	auto n12{ n11 /2 };
-	auto n21{ s2.length() };
-	auto n22{ n21 /2 };
-	if (n11 > n12) {
-		string newstring(n11-n21,'0');
-		s2 = newstring + s2;
-		cout << s2 << "\n";
+	big tenN{ tenToTheN(n1) };
+	big ten2n{ tenToTheN2(n1) };
+	big a, b, c, d;
+	splitter(n1, a, b);
+	splitter(n2, c, d);
+	big ac{ Karatsuba(a,c) };
+	big bd{ Karatsuba(b,d) };
+	big ad{ Karatsuba(a,d) };
+	big bc{ Karatsuba(b,c) };
+	return (tenN * ac) + (ten2n * (ad + bc)) + bd;
+}
+
+big Karatsuba2(big n1, big n2) {
+	if (digitsIn(n1) == 1 && digitsIn(n2) == 1) {
+		return n1 * n2;
 	}
-	else if (n12 > n11) {
-		string newstring(n21 - n11, '0');
-		s1 = newstring + s1;
-		cout << s2 << "\n";
+
+	big tenN{ tenToTheN(n1) };
+	big ten2n{ tenToTheN2(n1) };
+	big a, b, c, d;
+	splitter(n1, a, b);
+	splitter(n2, c, d);
+	big ac{ Karatsuba(a,c) };
+	big bd{ Karatsuba(b,d) };
+	big ad{ Karatsuba(a,d) };
+	big bc{ Karatsuba(b,c) };
+	return (tenN * ac) + (ten2n * (ad + bc)) + bd;
+}
+
+long long K(vector<int> x, vector<int> y, int n) {
+
+	if (n == 1) {
+		cout << "-> " << x[0] << " & " << y[0] << "\n";
+		return x[0] * y[0];
 	}
-	string high1{ s1.substr(0, n12) };
-	string low1{ s1.substr(n12,n12) };
-	string high2{ s2.substr(0,n22) };
-	string low2{ s2.substr(n22,n22) };
-	unsigned long long result1 = power(n11) * Karatsuba(high1, high2);
-	unsigned long long result2 = (power(n12)*((Karatsuba(high1, low2) + Karatsuba(low1, high2))));
-	unsigned long long result3 = Karatsuba(low1, low2);
-	if (result1 > LONG_MAX) cout << "Overflow risk on result 1\n";
-	if (result2 > LONG_MAX) cout << "Overflow risk on result 2\n";
-	if (result3 > LONG_MAX) cout << "Overflow risk on result 3\n";
-	cout << fixed << setprecision(0);
-	unsigned long long result = result1 + result2 + result3; 
-	cout << "Result = " << result << "\n";
+	int l = n / 2;
+	vector<int> a(l, 0);
+	vector<int> b(l, 0);
+	vector<int> c(l, 0);
+	vector<int> d(l, 0);
+
+	for (int i{ 0 }; i < l; ++i) {
+		a[i] = x[i];
+		b[i] = x[i + l];
+		c[i] = y[i];
+		d[i] = y[i + l];
+	}
+
+	long long U = K(a, c, l);
+	long long V = K(a, d, l);
+	long long W = K(b, c, l);
+	long long X = K(b, d, l);
+	long long result = (U * tenToTheN(n)) + (tenToTheN(l) * (V + W)) + X;
+	cout << result << "\n";
 	return result;
 }
 
 int main()
 {
+	vector<int> X(s1.size(), 0);
+	vector<int> Y(s2.size(), 0);
 
-	auto k = Karatsuba(s1, s2);
+	for (int i{ 0 }; i < s1.size(); ++i) {
+		string a{ s1[i] };
+		X[i] = atoi(a.c_str());
+		string b{ s2[i] };
+		Y[i] = atoi(b.c_str());
+	}
 
-	stringstream ss1{ s1 };
-	stringstream ss2{ s2 };
-	unsigned long long x1;
-	unsigned long long x2;
-	ss1 >> x1;
-	ss2 >> x2;
-	unsigned long long result{ x1 * x2 };
-	cout << fixed << setprecision(0); 
-	cout << " Check = " << result << endl;
-	if (result != k) cout << "Error\n";
+	cout << K(X, Y, s1.size());
+
 	return 0;
 }
 
