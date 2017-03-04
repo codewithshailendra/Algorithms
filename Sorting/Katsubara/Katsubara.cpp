01627bf6309eb62bfff64a391dd98f3efebcdbc0
Nick Thornton <n.419@live.com>
2017-03-04 07:28:01 +0000

    Works up t limit of long long

--- Sorting/Katsubara/Katsubara.cpp
+++ Sorting/Katsubara/Katsubara.cpp
@@ -12,26 +12,8 @@ using big = long double;
 
 string ss1{ R"(3141592653589793238462643383279502884197169399375105820974944592)" };
 string ss2{ R"(2718281828459045235360287471352662497757247093699959574966967627)" };
-string s1{ R"(1234)" };
-string s2{ R"(4321)" };
-
-/*
-
-function multiply(x; y)
-Input: Positive integers x and y, in binary
-Output: Their product
-n = max(size of x, size of y)
-if n = 1: return xy
-xL, xR = leftmost dn=2e, rightmost bn=2c bits of x
-yL, yR = leftmost dn=2e, rightmost bn=2c bits of y
-P1 = multiply(xL; yL)
-P2 = multiply(xR; yR)
-
-P3 = multiply(xL + xR; yL + yR)
-
-return P1 * 2^n + (P3 - P1 - P2)  2^(n/2) + P2
-
-*/
+string s1{ R"(12345678123456781234567812345678)" };
+string s2{ R"(12345678123456781234567812345678)" };
 
 unsigned power(unsigned x) {
 	int p{ 1 };
@@ -44,20 +26,21 @@ void splitter(big number, big & a, big & b) {
 	a = 0; b = 0;
 	stringstream ss;
 	ss << number;
-	string s{ ss.str() };
+	string s;
+	ss >> fixed >> setprecision(0) >> s; 
 	size_t half{ s.length() };
 	half = half / 2 + (half % 2);
 	string aString(s.substr(0, half));
 	string bString(s.substr(half));
-	cout << fixed << setprecision(0);
+	//cout << fixed << setprecision(0);
 	//cout << s << " => " << aString << " & " << bString << endl;
 	ss.clear();
 	ss.str(aString);
-	ss >> a;
+	ss >> fixed >> setprecision(0) >> a;
 	ss.clear();
 	ss.str(bString);
 	ss >> b;
-	cout << fixed << setprecision(0);
+	//cout << fixed << setprecision(0);
 	//cout << number << " => " << a << " & " << b << endl;
 }
 
@@ -85,7 +68,7 @@ big digitsIn(big number) {
 }
 
 big Karatsuba(big n1, big n2) {
-	if (digitsIn(n1) == 1 && digitsIn(n2) == 1) {
+	if (n1 > 9 && n2 > 9) {
 		return n1 * n2;
 	}
 
@@ -102,7 +85,7 @@ big Karatsuba(big n1, big n2) {
 }
 
 big Karatsuba2(big n1, big n2) {
-	if (digitsIn(n1) == 1 && digitsIn(n2) == 1) {
+	if (n1 > 9 && n2 > 9) {
 		return n1 * n2;
 	}
 
@@ -111,14 +94,14 @@ big Karatsuba2(big n1, big n2) {
 	big a, b, c, d;
 	splitter(n1, a, b);
 	splitter(n2, c, d);
-	big ac{ Karatsuba(a,c) };
-	big bd{ Karatsuba(b,d) };
-	big ad{ Karatsuba(a,d) };
-	big bc{ Karatsuba(b,c) };
-	return (tenN * ac) + (ten2n * (ad + bc)) + bd;
+	big z1{ Karatsuba2(a,c) };
+	big z2{ Karatsuba2(b,d) };
+	big z3{ Karatsuba2(a + b, c + d) };
+	big z4{ z3 - z1 - z2 };
+	return (tenN * z1) + (ten2n * z4) + z2;
 }
 
-long long K(vector<int> x, vector<int> y, int n) {
+big K(vector<int> x, vector<int> y, int n) {
 
 	if (n == 1) {
 		cout << "-> " << x[0] << " & " << y[0] << "\n";
@@ -137,28 +120,32 @@ long long K(vector<int> x, vector<int> y, int n) {
 		d[i] = y[i + l];
 	}
 
-	long long U = K(a, c, l);
-	long long V = K(a, d, l);
-	long long W = K(b, c, l);
-	long long X = K(b, d, l);
-	long long result = (U * tenToTheN(n)) + (tenToTheN(l) * (V + W)) + X;
+	big U = K(a, c, l);
+	big V = K(a, d, l);
+	big W = K(b, c, l);
+	big X = K(b, d, l);
+	big result = (U * tenToTheN(n)) + (tenToTheN(l) * (V + W)) + X;
 	cout << result << "\n";
 	return result;
 }
 
 int main()
 {
-	vector<int> X(s1.size(), 0);
-	vector<int> Y(s2.size(), 0);
-
-	for (int i{ 0 }; i < s1.size(); ++i) {
-		string a{ s1[i] };
-		X[i] = atoi(a.c_str());
-		string b{ s2[i] };
-		Y[i] = atoi(b.c_str());
-	}
-
-	cout << K(X, Y, s1.size());
+	cout << fixed << setprecision(0); 
+	//cout << "Max value long long is " << LDBL_MAX << endl;
+	big A;
+	big B;
+	stringstream ssa{ s1 };
+	ssa << fixed << setprecision(0);
+	ssa >> A;
+	stringstream ssb{ s1 };
+	ssb << fixed << setprecision(0);
+	ssb >> B;
+	cout << fixed << setprecision(0);
+	cout << A << endl;
+	cout << Karatsuba(A, B) << endl;
+	cout << Karatsuba2(A, B) << endl;
+	cout << A * B << endl;
 
 	return 0;
 }
